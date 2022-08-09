603ac39604b86c6f8d38f2effd2444e596276f93
Bomenezes <bomenezes83@gmail.com>
2022-08-09 18:32:49 -0300

    exercícios e fundamentos js

---
+++ funcoes.js
@@ -0,0 +1,69 @@
+
+
+
+var i = 1; 
+
+while (i < 10) { 
+
+   console.log('teste'); 
+
+   i++; 
+
+} 
+
+
+// const termoEscolhido = 'otorrinolaringologista';
+
+// let palavraInvertida = '';
+
+// function invertePalavra(termo) {
+
+//     for(let i = termo.length - 1; i >= 0; i--) {
+//         palavraInvertida = palavraInvertida + termo[i];
+//     }
+//     return palavraInvertida
+// }
+
+// console.log(invertePalavra(termoEscolhido));
+
+
+
+
+
+// let lista = [1,13,56,88,32];
+
+// console.log(delete lista[2]);
+// lista[2] = "oia eu aqui!";
+
+// lista.push('vaca');
+// lista.pop(lista[lista.length - 1]);
+// lista[lista.length] = 'bosta';
+// console.log(lista);
+
+// function mediaNotas(n1,n2,n3) {
+
+//     return (n1+n2+n3)/3;
+    
+// }
+
+// console.log(mediaNotas(8,7.5,10));
+
+
+
+// function soma() {
+//     return 2 + 2;
+// }
+
+// console.log(soma());
+
+// console.log(cabelo,chinelo);
+
+// let cabelo = 10;
+// // console.log(cabelo, chinelo);
+// {
+//     cabelo;
+//     let chinelo = 5;
+//     console.log(cabelo,chinelo);
+// }
+
+// console.log(cabelo, chinelo);
---
+++ tipos.js
@@ -0,0 +1,24 @@
+
+// const pi = 17;
+// let idade = 42;
+// console.log(respostaDeTudo, idade, pi)
+
+// {
+//     var respostaDeTudo = 3.14
+//     let idade = 38;
+//     pi;
+//     console.log(respostaDeTudo, idade, pi)
+
+// }
+// const op1 = 'casa';
+// const op2 = 'ap';
+// const conclusao = `A idade de ${idade} permite adquirir ${idade >= 45 ? op1 : op2}.`
+
+// console.log(conclusao);
+
+let x = 6;
+let y = 6;
+
+let compara = x % y
+
+console.log(compara)
